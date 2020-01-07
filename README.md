229878be7425ff2808367a70bd384f3917c750f1
1jtp8sobiu <3oxfoq0y@gmail.com>
2020-01-07 23:33:21 +0900

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # add_title_id_bat
+
+バブルの情報メニューに"TitleID"を追加  
+
+1. [batファイル](https://github.com/1jtp8sobiu/add_title_id_bat/archive/master.zip)をダウンロード
+2. ur0:shell/db/app.db をPCにコピー
+3. コピーした app.db を add_title_id.bat にドラッグ&ドロップ
+4. app.db をVitaに書き戻し再起動
+
+https://i.imgur.com/SwaQY2J.jpg
---
+++ add_title_id.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d %~dp0
+
+rem "TITLE"(3552295351) keyが作成された際に、TitleIDを付加して"UPDATE"
+bin\sqlite\sqlite3.exe "%1" "CREATE TRIGGER tgr_ADD_TitleID_TO_TITLE AFTER INSERT ON tbl_appinfo WHEN NEW.key LIKE '572932585' BEGIN UPDATE tbl_appinfo  SET val = '[' || new.titleId || '] ' || val  WHERE titleId = new.titleId AND key='572932585'; END;"
+
+rem "TITLE_00"(3552295351) keyが作成された際に、TitleIDを付加して"UPDATE"
+bin\sqlite\sqlite3.exe "%1" "CREATE TRIGGER tgr_ADD_TitleID_TO_TITLE_00 AFTER INSERT ON tbl_appinfo WHEN NEW.key LIKE '3475122802' BEGIN UPDATE tbl_appinfo  SET val = '[' || new.titleId || '] ' || val  WHERE titleId = new.titleId AND key='3475122802'; END;
+
+echo 完了。app.dbをVitaに書き戻して再起動。
+pause
