2dc7e452dcc2554dbd42906d6fb7c8bf363279e9
1jtp8sobiu <3oxfoq0y@gmail.com>
2020-01-07 23:51:22 +0900

    upload bat

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 1. [batファイル](https://github.com/1jtp8sobiu/add_title_id_bat/archive/master.zip)をダウンロード
 2. ur0:shell/db/app.db をPCにコピー
 3. コピーした app.db を add_title_id.bat にドラッグ&ドロップ
-4. app.db をVitaに書き戻し再起動
+4. app.db をVitaに書き戻す
+5. データベースを再構築するために、ux0:id.dat を開き の"MID:="の行を削除して再起動 
 
 https://i.imgur.com/SE8Da4H.jpg
--- add_title_id.bat
+++ add_title_id.bat
@@ -7,5 +7,8 @@ bin\sqlite\sqlite3.exe "%1" "CREATE TRIGGER tgr_ADD_TitleID_TO_TITLE AFTER INSER
 rem "TITLE_00"(3552295351) keyが作成された際に、TitleIDを付加して"UPDATE"
 bin\sqlite\sqlite3.exe "%1" "CREATE TRIGGER tgr_ADD_TitleID_TO_TITLE_00 AFTER INSERT ON tbl_appinfo WHEN NEW.key LIKE '3475122802' BEGIN UPDATE tbl_appinfo  SET val = '[' || new.titleId || '] ' || val  WHERE titleId = new.titleId AND key='3475122802'; END;"
 
-echo 完了。app.dbをVitaに書き戻して再起動。
+echo 処理完了。
+echo 1. app.db をVitaに書き戻す
+echo 2. データベースを再構築するために、ux0:id.dat を開き の"MID:="の行を削除して再起動
+echo. 
 pause
